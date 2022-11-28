7ed1c2d6866ca5df4b4964222818a40a4a78003f
DELL <DELL@DESKTOP-RRGV49A>
2022-11-28 22:47:52 +0000

    uploading to eclipse

---
+++ src/test/java/Test18_Linktext.java
@@ -0,0 +1,20 @@
+// Program 18 - write a program for link text locator any website
+
+package org.octtestingbatch.vas;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test18_Linktext
+{
+
+public static void main(String[] args) throws Throwable 
+{
+	
+	System.out.println("Wish you all the best automation stars");
+}
+}
